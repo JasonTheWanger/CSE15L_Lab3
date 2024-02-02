e1c7611f2f4b2ba9bea3bea35549d40a114c8c86
JasonTheWanger <jasonjuranwang@gmail.com>
2024-02-01 17:19:22 -0800

    Fixed both reverse mthod bugs

--- ArrayTests.java
+++ ArrayTests.java
@@ -15,4 +15,17 @@ public class ArrayTests {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
   }
+
+  @Test
+  public void testReversedVer1(){
+    int [] input = {1, 2, 3};
+    assertArrayEquals(new int[]{3,2,1}, ArrayExamples.reversed(input));
+  }
+
+  @Test
+  public void reverseInPlaceVer1(){
+    int [] input = {1, 2, 3};
+    ArrayExamples.reverseInPlace(input);
+    assertArrayEquals(new int []{1,2,3}, input);
+  }
 }
